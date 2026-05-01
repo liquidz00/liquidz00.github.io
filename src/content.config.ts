ef0152b9f18f1afb721decbcc9e81f10448caa73
Andrew Lerman <aspeciale88@gmail.com>
2026-05-01 17:58:01 -0400

    feat: add astro components

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+  loader: glob({
+    base: './src/content/blog',
+    pattern: '**/*.{md,mdx}',
+  }),
+  schema: z.object({
+    title: z.string(),
+    date: z.coerce.date(),
+    author: z.string().optional(),
+    description: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog };
